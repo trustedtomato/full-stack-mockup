b2f587cc0b94150845f70e8e444d2c53aee679aa
Tamás Halasi <trusted.tomato@gmail.com>
2022-02-13 14:25:31 +0100

    feat: set title according to the active route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { routes } from './app-routing.module'
+import { name } from '../config'
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,26 @@ import { routes } from './app-routing.module'
 })
 export class AppComponent {
   routes = routes
+
+  constructor (
+    private router: Router,
+    private titleHandler: Title,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit () {
+    // Keep title in sync with the route's data property.
+    // We don't have to worry about unsubscribing
+    // because this component will never get destroyed.
+    this.router.events.subscribe((data) => {
+      if (data instanceof NavigationEnd) {
+        let route = this.activatedRoute
+        while (route.firstChild) {
+          route = route.firstChild
+        }
+        const title = route.snapshot.data?.['title'] || ''
+        this.titleHandler.setTitle(`${title} – ${name}`)
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +18,9 @@ import { CitizensComponent } from './citizens/citizens.component';
     AppRoutingModule,
     StoreModule.forRoot({}, {})
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
